e232da99380eb609b878cf47688d1f2b1f33dffa
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-03 16:00:01 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ console.log("WHY ARE YOU LOOKING HERE?")
 async function request(file,id){
   let x = await fetch (file);
   let y = await x.text();
+  y.replace(/&quot;/ig,'"');
   console.log(y[0])
   const myArr = JSON.parse(y);
   console.log(myArr)
