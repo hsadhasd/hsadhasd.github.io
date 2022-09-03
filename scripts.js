0c07d84a3a43988c2ee2f6bad6d6cab1d598dfcf
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-03 14:58:27 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@ console.log("WHY ARE YOU LOOKING HERE?")
 async function request(file,id){
   let x = await fetch (file);
   let y = await x.text();
+  let z;
   for (const [key, value] of Object.entries(JSON.parse(y))) {
+    z = z + key
     console.log(key, value);
   }
  document.getElementById(id).innerHTML = "check console"
