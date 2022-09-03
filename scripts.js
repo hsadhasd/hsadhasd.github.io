7c272505685405c5605104676a23138c0e505830
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-03 15:00:50 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 console.log("WHY ARE YOU LOOKING HERE?")
 async function request(file,id){
   let x = await fetch (file);
-  let y = await x.text();
+  let y = await JSON.parse(x.text());
   let z;
-  for (const [key, value] of Object.entries(JSON.parse(y))) {
+  for (const [key, value] of Object.entries(y)) {
     z = z + key
     console.log(key, value);
   }
+ console.log(z)
  document.getElementById(id).innerHTML = "check console"
 }
 request("api/RustSkinPrices.txt","demo")  
