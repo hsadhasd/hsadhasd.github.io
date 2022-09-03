ebb7ef6aaeb546bfb59575351a64e25a1ec95806
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-03 14:37:18 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ console.log("WHY ARE YOU LOOKING HERE?")
 async function request(file,id){
   let x = await fetch (file);
   let y = await x.text();
-  document.getElementById(id).innerHTML = y;
+  for (const [key, value] of Object.entries(y)) {
+  console.log(key, value);
+  }
 }
 request("api/RustSkinPrices.txt","demo")  
