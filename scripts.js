9ac6af3224228afe3ea1fb1665f2b83277497c51
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-03 14:26:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 async function request(file,id){
   let x;
-  x = fetch (file)then(y => document.getElementById(id).innerHTML = y);;
-  x = x.text()
-  document.getElementById(id).innerHTML = x
+  x = fetch (file);
+  x = x.text();
+  document.getElementById(id).innerHTML = x;
 }
 request("api/RustSkinPrices.txt","demo")  
