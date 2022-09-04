1f0c08bce486d2f09986faf6d6b21228cb8acbac
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-04 20:17:42 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ async function request_1(file,id){
   for (g in myArr) {
     let name = myArr[g].name;
     nameE = name.replace(/ /g,"%20");
+    name = name.replace(/"/g,"\"")
     let link = "https://steamcommunity.com/market/listings/252490/"+nameE;
   	txt = txt + "<tr>" + "<td><a target=_blank href="+link+">" + name + "</a></td>"+ "<td>$" + myArr[g].price + "</td>" + "</tr>"; 
   }
