b27bd57e0c20e9ada1c9644988ce45d9a7a770ff
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-06 19:34:29 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,12 +19,13 @@ async function request_1(file,id){
 var $rows = $('#table tr');
 $('#search').keyup(function() {
 
-    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-        reg = RegExp(val, 'i'),
-        text;
+  var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+    reg = RegExp(val, 'i'),
+    text;
 
-    $rows.show().filter(function() {
-        text = $(this).text().replace(/\s+/g, ' ');
-        return !reg.test(text);
-    }).hide();
+  $rows.show().filter(function() {
+    text = $(this).text().replace(/\s+/g, ' ');
+    return !reg.test(text);
+  }).hide();
 });
+
