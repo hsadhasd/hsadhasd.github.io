f9943acb6a92c4f8b56d1b94a0be8977e000111b
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-06 19:42:25 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,20 +12,7 @@ async function request_1(file,id){
     nameE = name.replace(/ /g,"%20");
     name = name.replace(/%27/g,"\'")
     let link = "https://steamcommunity.com/market/listings/252490/"+nameE;
-  	txt = txt + "<tr>" + "<td>"+name+"</td>"+ "<td>$" + myArr[g].sitePrice + "</td>" + "<td>$" + myArr[g].discordPrice + "</td>" + "</tr>"; 
+  	txt = txt + "<tr>" + "<td><a target=_blank href="+link+">"+ name + "</a></td>"+ "<td>$" + myArr[g].sitePrice + "</td>" + "<td>$" + myArr[g].discordPrice + "</td>" + "</tr>"; 
   }
- document.getElementById(id).innerHTML = "<input type=text id=search placeholder=Type to search> <table id=table>" + txt + "</table>";
+ document.getElementById(id).innerHTML = "<table>" + txt + "</table>";
 }
-var $rows = $('#table tr');
-$('#search').keyup(function() {
-
-  var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-    reg = RegExp(val, 'i'),
-    text;
-
-  $rows.show().filter(function() {
-    text = $(this).text().replace(/\s+/g, ' ');
-    return !reg.test(text);
-  }).hide();
-});
-
