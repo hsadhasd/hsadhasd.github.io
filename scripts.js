ad04ff53706d469ab11b169044027784cf159577
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-11 21:59:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,13 +7,13 @@ async function request_1(file,id){
   let z = y.replace(/'/g, '"');
   const myArr = JSON.parse(z);
   let txt = ""
-  txt = txt + "<tr>" + "<th>" + "Name" + "</th>"+ "<th>" + "Buy" + "</th>"  + "<th>" + "Sell" + "</th>" + "<th>" + "Stock" + "</th>" + "</tr>"; 
+  txt = txt + "<tr>" + "<th>" + "Num" + "</th>" + "<th>" + "Name" + "</th>"+ "<th>" + "Buy" + "</th>"  + "<th>" + "Sell" + "</th>" + "<th>" + "Stock" + "</th>" + "</tr>"; 
   for (g in myArr) {
     let name = myArr[g].name;
     nameE = name.replace(/ /g,"%20");
     name = name.replace(/%27/g,"\'")
     let link = "https://steamcommunity.com/market/listings/252490/"+nameE;
-  	txt = txt + "<tr>" + "<td><a target=_blank href="+link+">"+ name + "</a></td>"+ "<td>$" + myArr[g].buy + "</td>"+ "<td>$" + myArr[g].sell + "</td>"+ "<td>" + myArr[g].stock + "</td>"+"</tr>"; 
+  	txt = txt + "<tr>" "<td>" + g + "</td>" + "<td><a target=_blank href="+link+">"+ name + "</a></td>"+ "<td>$" + myArr[g].buy + "</td>"+ "<td>$" + myArr[g].sell + "</td>"+ "<td>" + myArr[g].stock + "</td>"+"</tr>"; 
   }
  document.getElementById(id).innerHTML = "<table>" + txt + "</table>";
 }
