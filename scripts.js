d4bca1a58ceaf88ea73adb9e1d8fef679fcd6031
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-17 16:44:29 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ async function request_1(file,id){
     nameE = name.replace(/ /g,"%20");
     name = name.replace(/%27/g,"\'")
     let link = "https://steamcommunity.com/market/listings/252490/"+nameE;
-  	txt = txt + "<tr>" + "<td>" + g + "</td>" + "<td><a target=_blank href="+link+">"+ name + "</a></td>"+ "<td>$" + myArr[g].buy + "</td>"+ "<td>$" + myArr[g].sell + "</td>"+ "<td>" + myArr[g].stock + "</td>"+"</tr>"; 
+  	txt = txt + "<tr>" + "<td>" + g + "</td>" + "<td><a target=_blank href="+link+">"+ name + "</a></td>"+ "<td>$" + myArr.items[g].buy + "</td>"+ "<td>$" + myArr.items[g].sell + "</td>"+ "<td>" + myArr.items[g].stock + "</td>"+"</tr>"; 
   }
  document.getElementById(id).innerHTML = "<table>" + txt + "</table>";
 }
