1b1c9f01e3f34a9c8099fa74b279a342942f4693
hsadhasd <96871101+hsadhasd@users.noreply.github.com>
2022-09-25 00:15:15 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,21 +31,6 @@ async function request_2(file,id){
   else {
     txt = txt + "<a title=Offline style=color:red target=_blank href=https://cs.deals/market/rust/>cs.deals</a>("+"<a title=Balance on cs.deals>$"+myArr.csdeals.balance+"</a>"+")/"
   }
-  if (myArr.dmarket.status === "True") {
-    txt = txt + "<a title=Online style=color:green target=_blank href=https://dmarket.com/ingame-items/item-list/rust-skins>dmarket.com</a>("+"<a title=Balance on dmarket.com>$"+myArr.dmarket.balance+"</a>"+")/"
-  }
-  else {
-    txt = txt + "<a title=Offline style=color:red target=_blank href=https://dmarket.com/ingame-items/item-list/rust-skins>dmarket.coms</a>("+"<a title=Balance on dmarket.com>$"+myArr.dmarket.balance+"</a>"+")/"
-  }
-  if (myArr.skinport.status === "True") {
-    txt = txt + "<a title=Online style=color:green target=_blank href=https://skinport.com/rust>skinport.com</a>("+"<a title=Balance on skinport.com>$"+myArr.skinport.balance+"</a>"+")"
-  }
-  else {
-    txt = txt + "<a title=Offline style=color:red target=_blank href=https://skinport.com/rust>skinport.com</a>("+"<a title=Balance on skinport.com>$"+myArr.skinport.balance+"</a>"+")"
-  }
-  document.getElementById(id).innerHTML = "<p>"+txt+"</p>";
-}
-
 async function request_refresh(id){
   console.log(id)
   document.getElementById(id).innerHTML = "Loading...";
